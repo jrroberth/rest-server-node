4d639b56bc03c6181eda83dcd9df5a197c8b9a02
Robert <robert17moran@gmail.com>
2020-11-03 23:22:52 -0500

    variable d entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,6 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
-    urlDB = 'mongodb+srv://rko:FrP8NlfVc3PMbm31@cluster0.odoec.mongodb.net/cafe';
 }
 
 // process.env.BASE = 'mongodb://localhost:27017/cafe';
