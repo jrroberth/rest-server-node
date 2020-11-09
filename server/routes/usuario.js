7ee4ccdee843d0f13a2e3d2cc0e67c9bc452e78c
Robert <robert17moran@gmail.com>
2020-11-08 23:36:43 -0500

    para ingresar usuario3

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -36,7 +36,7 @@ app.get('/usuario', verificaToken, (req, res) => {
 
 });
 
-app.post('/usuario', [verificaToken], (req, res) => {
+app.post('/usuario', (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
